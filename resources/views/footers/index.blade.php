0fe1ee7916d141e494fdebd342fdbb1542f2850e
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-10-01 14:11:34 +0200

    navbar back finie

--- resources/views/footers/index.blade.php
+++ resources/views/footers/index.blade.php
@@ -10,83 +10,88 @@
     </div>
 
 
-    <table class="table mt-5 container">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nom </th>
-                <th scope="col">Adresse </th>
-                <th scope="col">Ville</th>
-                <th scope="col">Telephone</th>
-                <th scope="col">Num Tel</th>
-                <th scope="col">Email</th>
-                <th scope="col">Adresse Email</th>
-                <th scope="col">Links</th>
-                <th scope="col">Link 1</th>
-                <th scope="col">Link 2</th>
-                <th scope="col">Link 3</th>
-                <th scope="col">Link 4</th>
-                <th scope="col">Link 5</th>
-                <th scope="col">Services</th>
-                <th scope="col">Link 6</th>
-                <th scope="col">Link 7</th>
-                <th scope="col">Link 8</th>
-                <th scope="col">Link 9</th>
-                <th scope="col">Link 10</th>
-                <th scope="col">Newsletter</th>
-                <th scope="col">Text Newsletter</th>
-                <th scope="col">Copyright 1</th>
-                <th scope="col">Copyright 2</th>
-                <th scope="col">Copyright 3</th>
-                <th scope="col">Credits 1</th>
-                <th scope="col">Credits 2</th>
-                <th scope="col">Actions</th>
-            </tr> 
-        </thead>
-        <tbody>
-            @foreach ($footer as $data)
-                <tr>
-                    <th scope="row">{{ $data->id }}</th>
-                    <td>{{ $data->nom }}</td>
-                    <td>{{ $data->adress }}</td>
-                    <td>{{ $data->ville }}</td>
-                    <td>{{ $data->tel }}</td>
-                    <td>{{ $data->numtel }}</td>
-                    <td>{{ $data->email }}</td>
-                    <td>{{ $data->emailadress  }}</td>
-                    <td>{{ $data->links }}</td>
-                    <td>{{ $data->link1 }}</td>
-                    <td>{{ $data->link2 }}</td>
-                    <td>{{ $data->link3 }}</td>
-                    <td>{{ $data->link4 }}</td>
-                    <td>{{ $data->link5  }}</td>
-                    <td>{{ $data->services }}</td>
-                    <td>{{ $data->link6 }}</td>
-                    <td>{{ $data->link7 }}</td>
-                    <td>{{ $data->link8 }}</td>
-                    <td>{{ $data->link9 }}</td>
-                    <td>{{ $data->link10  }}</td>
-                    <td>{{ $data->newsletter }}</td>
-                    <td>{{ $data->newsletterdescription }}</td>
-                    <td>{{ $data->copy1 }}</td>
-                    <td>{{ $data->copy2 }}</td>
-                    <td>{{ $data->copy3 }}</td>
-                    <td>{{ $data->credits }}</td>
-                    <td>{{ $data->credits2 }}</td>
-                    <td class="d-flex">
-                        <a href="{{ route('footers.edit', $data->id) }}" class="btn btn-info mx-2">Edit</a>
-                        <a href="{{ route('footers.show', $data->id) }}" class="btn btn-warning mr-2">Show</a>
+    <!-- ======= Footer ======= -->
+<footer id="footer" class="mt-5">
+    
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>Bikin</h3>
+                    <p>
+                        A108 Adam Street <br>
+                        New York, NY 535022<br>
+                        United States <br><br>
+                        <strong>Phone:</strong> +1 5589 55488 55<br>
+                        <strong>Email:</strong> info@example.com<br>
+                    </p>
+                </div>
+                
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+                
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                    </ul>
+                </div>
+                
+                <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Join Our Newsletter</h4>
+                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
+    <div class="container d-md-flex py-4">
+        
+        <div class="me-md-auto text-center text-md-start">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Bikin</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bikin-free-simple-landing-page-template/ -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+        </div>
+        <div class="social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="bx bxl-google-plus"></i></a>
+            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
 
-                        <form action="{{ route('footers.destroy', $data->id) }}" method="post">
-                            @method('DELETE')
-                            @csrf
-                            <button class="btn btn-danger mx-2">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
 
+@foreach ($footer as $data)
+    
+<div class="d-flex justify-content-center mt-5">
+    <a href="{{ route('footers.edit', $data->id) }}" class="btn btn-info mx-2">Edit</a>
+</div>
+@endforeach
 
 @endsection
